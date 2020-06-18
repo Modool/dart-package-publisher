6d5a0131f312f80e6f5da1532362d53ab742de1f
xulinfeng <xulinfeng@bilibili.com>
2020-06-18 19:29:29 +0800

    Updated shell.

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,107 +30,45 @@ switch_working_directory() {
   echo "Package dir: $PWD"
 }
 
-get_local_package_version() {
-  GET_OUTPUT=`flutter pub get`
-  DEPS_OUTPUT=`flutter pub deps`
+publish() {
+  flutter doctor
 
-  PACKAGE_INFO=`echo "$DEPS_OUTPUT" | cut -d'|' -f1 | cut -d"'" -f1 | sed '/^\s*$/d'`
-  IFS=$'\n\r' read -d '' -r -a lines <<< "$PACKAGE_INFO"
-  lastIndex=`expr ${#lines[@]}-1`
-  echo "$PACKAGE_INFO"
-  DART_VERSION=`echo "$PACKAGE_INFO" | perl -n -e'/^Dart SDK (.*)$/ && print $1'`
-  FLUTTER_VERSION=`echo "$PACKAGE_INFO" | perl -n -e'/^Flutter SDK (.*)$/ && print $1'`
-  PACKAGE_INFO="${lines[$lastIndex]}"
-  PACKAGE=`echo "$PACKAGE_INFO" | cut -d' ' -f1`
-  LOCAL_PACKAGE_VERSION=`echo "$PACKAGE_INFO" | cut -d' ' -f2`
-  if [ -z "$PACKAGE" ]; then
-    echo "No package found. :("
+  mkdir -p ~/.pub-cache
+  if [ -z "$INPUT_CREDENTIALJSON" ]; then
+    cat <<-EOF > ~/.pub-cache/credentials.json
+    {
+      "accessToken":"$INPUT_ACCESSTOKEN",
+      "refreshToken":"$INPUT_REFRESHTOKEN",
+      "tokenEndpoint":"https://accounts.google.com/o/oauth2/token",
+      "scopes": [ "openid", "https://www.googleapis.com/auth/userinfo.email" ],
+      "expiration": 1577149838000
+    }
+EOF
+  else
+    echo "$INPUT_CREDENTIALJSON" > ~/.pub-cache/credentials.json
+  fi
+
+  flutter pub publish --dry-run
+
+  if [ $? -eq 0 ]; then
+    echo "Dry Run Successfull."
+  else
+    echo "Dry Run Failed, skip real publishing."
     exit 0
   fi
-  echo "::set-output name=dartVersion::$DART_VERSION"
-  if [ "$FLUTTER_VERSION" != "" ]; then
-    echo "::set-output name=flutterVersion::$FLUTTER_VERSION"
-  fi
-  echo "::set-output name=package::$PACKAGE"
-  echo "::set-output name=localVersion::$LOCAL_PACKAGE_VERSION"
-}
-
-run_unit_tests() {
-    if [ "$INPUT_SKIPTESTS" = "true" ]; then
-      echo "Skip unit tests set to true, skip unit testing."
-    else
-      HAS_BUILD_RUNNER=`echo "$DEPS_OUTPUT" | perl -n -e'/^.* build_runner (.*)/ && print $1'`
-      HAS_BUILD_TEST=`echo "$DEPS_OUTPUT" | perl -n -e'/^.* build_test (.*)/ && print $1'`
-      HAS_TEST=`echo "$DEPS_OUTPUT" | perl -n -e'/^.* (test|flutter_test) (.*)/ && print $2'`
-      if [ "$HAS_BUILD_RUNNER" != "" ] && [ "$HAS_BUILD_TEST" != "" ] && [ "$INPUT_SUPPRESSBUILDRUNNER" != "true" ]; then
-        echo "flutter tests with build_runner"
-        flutter pub run build_runner build
-        flutter test
-      else
-        if [ "$HAS_TEST" != "" ]; then
-          flutter test
-        else
-          echo "No unit test related dependencies detected, skip unit testing."
-        fi
-      fi
-    fi
-}
-
-get_remote_package_version() {
-  ACTIVATE_OUTPUT=`flutter pub global activate $PACKAGE`
-  REMOTE_PACKAGE_VERSION=`echo "$ACTIVATE_OUTPUT" | perl -n -e'/^Activated .* (.*)\./ && print $1'`
-  if [ -z "$REMOTE_PACKAGE_VERSION" ]; then
-    REMOTE_PACKAGE_VERSION="✗"
-  fi
-  echo "Local version: [$LOCAL_PACKAGE_VERSION]"
-  echo "Remote version: [$REMOTE_PACKAGE_VERSION]"
-  echo "::set-output name=remoteVersion::$REMOTE_PACKAGE_VERSION"
-}
-
-publish() {
-  if [ "$LOCAL_PACKAGE_VERSION" = "$REMOTE_PACKAGE_VERSION" ]; then
-    echo "Remote & Local versions are equal, skip publishing."
+  if [ "$INPUT_DRYRUNONLY" = "true" ]; then
+    echo "Dry run only, skip publishing."
   else
-    mkdir -p ~/.pub-cache
-    if [ -z "$INPUT_CREDENTIALJSON" ]; then
-      cat <<-EOF > ~/.pub-cache/credentials.json
-      {
-        "accessToken":"$INPUT_ACCESSTOKEN",
-        "refreshToken":"$INPUT_REFRESHTOKEN",
-        "tokenEndpoint":"https://accounts.google.com/o/oauth2/token",
-        "scopes": [ "openid", "https://www.googleapis.com/auth/userinfo.email" ],
-        "expiration": 1577149838000
-      }
-EOF
-    else
-      echo "$INPUT_CREDENTIALJSON" > ~/.pub-cache/credentials.json
-    fi
-
-    flutter pub publish --dry-run
+    flutter pub publish -f
 
     if [ $? -eq 0 ]; then
-      echo "Dry Run Successfull."
+      echo "::set-output name=success::true"
     else
-      echo "Dry Run Failed, skip real publishing."
-      exit 0
-    fi
-    if [ "$INPUT_DRYRUNONLY" = "true" ]; then
-      echo "Dry run only, skip publishing."
-    else
-      flutter pub publish -f
-
-      if [ $? -eq 0 ]; then
-        echo "::set-output name=success::true"
-      else
-        echo "::set-output name=success::false"
-      fi
+      echo "::set-output name=success::false"
     fi
   fi
 }
 
 check_required_inputs
 switch_working_directory
-get_local_package_version || true
-run_unit_tests
-get_remote_package_version || true
 publish || true
